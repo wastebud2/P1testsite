fa60ae55db49ac6b96b1e4bcb1ad556a41f47d63
wastebud2 <39827939+wastebud2@users.noreply.github.com>
2019-10-27 19:37:20 +0100

    Implemented basic API database update

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import pymongo
 
 ## Setup ##
@@ -10,9 +10,17 @@ db_entries = db["test_col"]
 ## Routes ##
 @app.route("/")
 def index():
-    #return str(db_entries.find_one({'address': 'Recycling (Mineralvej 23)'})['address'])
-
     return render_template('index.html', entries=db_entries)
+
+@app.route('/update/<int:pk>/<float:fill>/')
+def update(pk, fill):
+    try:
+        db_entries.update_one({'pk':pk},{ "$set": {'fill':fill}}, upsert=False)
+        return "Success!"
+    except:
+        return "An error occurred"
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)

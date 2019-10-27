877dc48ac6500c25967ba1da727c171422401b25
wastebud2 <39827939+wastebud2@users.noreply.github.com>
2019-10-27 18:44:31 +0100

    Large amount of basic functionality

--- application.py
+++ application.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+import pymongo
 
+## Setup ##
+app = Flask(__name__)
+mongoclient = pymongo.MongoClient("mongodb+srv://test:1234@test-wbvkk.azure.mongodb.net/test?retryWrites=true&w=majority") 
+db = mongoclient["testbase"]
+db_entries = db["test_col"]
+
+## Routes ##
 @app.route("/")
 def index():
-    return render_template('index.html')
+    #return str(db_entries.find_one({'address': 'Recycling (Mineralvej 23)'})['address'])
+
+    return render_template('index.html', entries=db_entries)
 
 if __name__ == "__main__":
     app.run(debug=True)

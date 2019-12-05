556bd554f60e507fb736d0d393a54e0b7bce9528
wastebud2 <39827939+wastebud2@users.noreply.github.com>
2019-12-05 09:55:13 +0100

    Converted sorting function to Firebase Firestore, and made a user interface for generating a route

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from app.travelingSalesman import sortLocations
 import json
 import firebase_admin
@@ -17,33 +17,62 @@ units_ref = db.collection('enheder')
 ## Routes ##
 @app.route("/")
 def index():
-    return render_template('index.html', entries=units_ref.stream())
+    # Sort by type, so 'Plast og Metal' de ligger i deres kategorier
+    data = units_ref.order_by('type').order_by('fill', direction=firestore.Query.DESCENDING)
+    return render_template('index.html', entries=data.stream())
 
-####### Do we still need this function?
-'''
-@app.route('/update/<int:pk>/<float:fill>/')
-def update(pk, fill):
-    try:
-        db_entries.update_one({'pk':pk},{ "$set": {'fill':fill}}, upsert=False)
-        return "Success!"
-    except:
-        return "An error occurred"
-'''
-
-###### Fix sort function
-'''
 @app.route("/sort/<float:fill>")
 def sort(fill):
     locs = []
-    for entry in db_entries.find():     
-        if entry["fill"] > fill:
-            new_entry = [entry["pk"], entry["lat"], entry["lng"]]
+    for entry in units_ref.stream():
+        entry_dict = entry.to_dict()
+        if entry_dict['fill'] > fill:
+            new_entry = [entry.id, entry_dict["lat"], entry_dict["lng"]]
             locs.append(new_entry)
     bestOrder = sortLocations(locs)
     locs = locs[::-1] # flip list order
     locs = [locs[i] for i in bestOrder]
     return render_template('sort.html', data=locs)
-'''
+
+@app.route("/sort/<string:type>/<float:fill>", methods=['POST', 'GET'])
+def sort_with_type(type, fill):
+    locs = []
+    for entry in units_ref.stream():
+        entry_dict = entry.to_dict()
+        if entry_dict['type'] == type and entry_dict['fill'] > fill:
+            new_entry = [entry.id, entry_dict["lat"], entry_dict["lng"]]
+            locs.append(new_entry)
+    bestOrder = sortLocations(locs)
+    locs = locs[::-1] # flip list order
+    locs = [locs[i] for i in bestOrder]
+    return render_template('sort.html', data=locs)
+
+@app.route("/sort/GUI", methods=['POST'])
+def sort_GUI():
+    if request.method == 'POST':
+        type = request.form['typeSelect']
+        fill = float(request.form['fillRange'])
+        locs = [] # List of locations to sort
+        for entry in units_ref.stream():
+            entry_dict = entry.to_dict()
+            # If no filter is selected ...
+            if type == 'NoFilter':
+                #... sort only by fill amount ...
+                if entry_dict['fill'] > fill:
+                    new_entry = [entry.id, entry_dict["lat"], entry_dict["lng"]]
+                    locs.append(new_entry)
+            #... else sort by fill amount only in chosen category/filter
+            else:
+                if entry_dict['type'] == type and entry_dict['fill'] > fill:
+                    new_entry = [entry.id, entry_dict["lat"], entry_dict["lng"]]
+                    locs.append(new_entry)
+        # Sort chosen locations
+        bestOrder = sortLocations(locs)
+        locs = locs[::-1] # flip list order
+        locs = [locs[i] for i in bestOrder] # Rearrange locs in the best order, found via algorithm
+        return json.dumps(locs)
+        #return render_template('sort.html', data=locs)
+
 
 if __name__ == "__main__":
     app.run(debug=True)

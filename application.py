cdb682b968db68488d0da8035522eb77488dc33f
wastebud2 <39827939+wastebud2@users.noreply.github.com>
2019-12-16 14:17:11 +0100

    Mange mindre ændringer af programmering og index.html

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, flash
 from app.brute_force import sortLocations
 from app.ACO import sort_ACO
 import json
@@ -14,38 +14,37 @@ firebase = firebase_admin.initialize_app(cred)
 db = firestore.client()
 units_ref = db.collection('enheder')
 
+aaf = ["Aalborg Forsyning", 57.026012, 9.921496]
 
 ## Routes ##
 @app.route("/")
 def index():
-    # Sort by type, so 'Plast og Metal' de ligger i deres kategorier
+    # Sort entries by type
     data = units_ref.order_by('type').order_by('fill', direction=firestore.Query.DESCENDING)
     return render_template('index.html', entries=data.stream())
 
 @app.route("/sort/<float:fill>")
 def sort(fill):
     locs = []
+    locs.append(aaf)
     for entry in units_ref.stream():
         entry_dict = entry.to_dict()
         if entry_dict['fill'] > fill:
             new_entry = [entry.id, entry_dict["lat"], entry_dict["lng"]]
             locs.append(new_entry)
-    locs =   sort_ACO(locs)
-    #locs = locs[::-1] # flip list order
-    #locs = [locs[i] for i in bestOrder]
+    locs = sort_ACO(locs)
     return json.dumps(locs)
 
 @app.route("/sort/<string:type>/<float:fill>")
 def sort_with_type(type, fill):
     locs = []
+    locs.append(aaf)
     for entry in units_ref.stream():
         entry_dict = entry.to_dict()
         if entry_dict['type'] == type and entry_dict['fill'] > fill:
             new_entry = [entry.id, entry_dict["lat"], entry_dict["lng"]]
             locs.append(new_entry)
-    locs =   sort_ACO(locs)
-    #locs = locs[::-1] # flip list order
-    #locs = [locs[i] for i in bestOrder]
+    locs = sort_ACO(locs)
     return json.dumps(locs)
 
 @app.route("/sort/GUI", methods=['POST'])
